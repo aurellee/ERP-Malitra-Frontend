82c509a4629c9c90cab36bbe2c87de79cc2e9daa
Jaqueline Aurelia L. <aurelialangi06@gmail.com>
2025-04-21 01:19:12 +0700

    feat: add invoiceDetail and integrating invoices Api to the frontend

--- app/(invoices)/invoices/page.tsx
+++ app/(invoices)/invoices/page.tsx
@@ -8,7 +8,6 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { DateRangePicker } from "@/components/date-range-picker"
 import Link from "next/link"
-import invoiceApi from "@/api/invoiceApi"
 import type { DateRange } from "react-day-picker"
 import {
   Search,
@@ -18,13 +17,23 @@ import {
   PencilLine,
   Trash2,
 } from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { useRouter } from "next/navigation"
-import { format } from "date-fns"
+import { format, parseISO } from "date-fns"
 import { parseRupiah } from "@/utils/commonFunctions"
 import { Invoice } from "@/types/types"
+import invoiceApi from "@/api/invoiceApi"
 
 // Konfigurasi pagination
-const ITEMS_PER_PAGE = 10
+const ITEMS_PER_PAGE = 9
 
 export default function InvoicesPage() {
   const [currentPage, setCurrentPage] = useState(1)
@@ -56,6 +65,15 @@ export default function InvoicesPage() {
   const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE)
 
   const [searchQuery, setSearchQuery] = useState("")
+
+  const [dialogDeleteOpen, setDialogDeleteOpen] = useState(false)
+  const [dialogEditOpen, setDialogEditOpen] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const [editIndex, setEditIndex] = useState<number | null>(null)
+  const [deleteIndex, setDeleteIndex] = useState<number | null>(null)
+
+  // const [form, setForm]
 
   useEffect(() => {
     handleViewAllInvoices();
@@ -95,7 +113,7 @@ export default function InvoicesPage() {
       const res = await invoiceApi().invoiceSummaryFilter(formDate);
       if (res.status == 200) {
         setSummary(res.data);
-      }  else {
+      } else {
         console.log("Error fetching data:", res.error)
       }
     } catch (error) {
@@ -113,24 +131,122 @@ export default function InvoicesPage() {
       }
     } catch (error) {
       console.log("Error fetching invoices list...");
-    } 
+    }
   }
-  
+
   const filterInvoicesByDate = () => {
     const fromDate = dateRange?.from ?? new Date();
     const toDate = dateRange?.to ?? new Date();
-  
+
     // Normalize dates to start and end of the day
     fromDate.setHours(0, 0, 0, 0);
-    toDate.setHours(23, 59, 59, 999); 
-  
+    toDate.setHours(23, 59, 59, 999);
+
     const filtered = invoices.filter(invoice => {
       const invoiceDate = new Date(invoice.invoice_date);
       return invoiceDate >= fromDate && invoiceDate <= toDate;
     });
-  
+
     setFilteredInvoices(filtered);
   }
+
+  const [form, setForm] = useState({
+    invoice_id: 0,
+    invoice_date: "",
+    total_price: 0,
+    amount_paid: 0,
+    payment_method: "",
+  })
+
+  const handleEditClick = (invoice: any, idx: number) => {
+    setEditIndex(idx)
+    setForm({
+      invoice_id: invoice.invoice_id,
+      invoice_date: invoice.invoice_date,
+      amount_paid: invoice.amount_paid,
+      total_price: invoice.total_price,
+      payment_method: invoice.payment_method,
+    })
+    setDialogEditOpen(true)
+  }
+
+  // PUT updated payload back to your API
+  const handleUpdateInvoice = async () => {
+    try {
+      if (editIndex !== null && editIndex !== undefined) {
+        const payload = {
+          invoice_id: invoices[editIndex].invoice_id,
+          invoice_date: form.invoice_date,
+          amount_paid: form.amount_paid,
+          total_price: form.total_price,
+          payment_method: form.payment_method,
+        }
+
+        const res = await invoiceApi().updateInvoice(payload)
+        if (res.error) throw new Error(res.error)
+        // success! re‑fetch and close
+        await handleViewAllInvoices()
+        setDialogEditOpen(false)
+        setEditIndex(null)
+        if (res.error) throw new Error(res.error)
+      } else {
+        console.error('editIndex is null or undefined');
+      }
+    } catch (err) {
+      console.error("Update failed:", err)
+
+    }
+  }
+
+  const resetForm = () => {
+    setForm({
+      invoice_id: 0,
+      total_price: 0,
+      invoice_date: "",
+      amount_paid: 0,
+      payment_method: "",
+    })
+  }
+
+  const isFormValid =
+    // form.invoice_id > 0 &&
+    // form.invoice_date.trim() !== "" &&
+    form.payment_method.trim() !== "" &&
+    form.amount_paid > 0
+
+  const handleDeleteInvoice = async () => {
+    if (deleteIndex === null) return
+
+    // Ambil produk yang akan di‐delete
+    const invoiceToDelete = invoices[deleteIndex]
+
+    try {
+      // Kirim payload yang benar: product_id dari productToDelete, bukan form.productID
+      const res = await invoiceApi().deleteInvoice({
+        invoice_id: invoiceToDelete.invoice_id,
+      })
+
+      if (res.error) {
+        throw new Error(res.error)
+      }
+
+      // Success: tutup dialog, reset index, dan refresh list
+      setDialogDeleteOpen(false)
+      setDeleteIndex(null)
+      await handleViewAllInvoices()
+    } catch (err) {
+      console.error("Failed to delete:", err)
+    }
+  }
+
+
+  const handleChange = (field: string, value: any) => {
+    setForm({
+      ...form,
+      [field]: value,
+    })
+  }
+
   // Next / Prev page
   function handleNextPage() {
     if (currentPage < totalPages) {
@@ -193,7 +309,7 @@ export default function InvoicesPage() {
           {/* Search */}
           <div className="relative flex items-center gap-6">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" size={16} />
-            <Input type="text" placeholder="Search..." className="pl-9 pr-5" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+            <Input type="text" placeholder="Search..." className="pl-9 pr-5" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
           </div>
 
           {/* Filter button */}
@@ -207,7 +323,7 @@ export default function InvoicesPage() {
             className="bg-[#0456F7] text-white hover:bg-[#0348CF]"
             onClick={() => router.push("/newOrder")}
           >
-            + Add Product
+            + Add Invoice
           </Button>
         </div>
       </div>
@@ -227,7 +343,7 @@ export default function InvoicesPage() {
               <th className="px-4 py-3 font-semibold">Payment</th>
               <th className="px-4 py-3 font-semibold">Status</th>
               <th className="px-4 py-3 font-semibold">Detail</th>
-              <th className="px-4 py-3 font-semibold"></th>
+              <th className="px-0 py-3 font-semibold"></th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100 text-gray-700 dark:bg-[#121212] dark:text-gray-300 dark:divide-[oklch(1_0_0_/_10%)]">
@@ -245,19 +361,150 @@ export default function InvoicesPage() {
                 {/* Detail column: link ke /invoices/[invoiceId] */}
                 <td className="px-4 py-3">
                   <Link
-                    href={`/invoices/${invoice.invoice_id}`}
+                    href={{
+                      pathname: '/invoiceDetail',
+                      query: { invoice_id: invoice.invoice_id }
+                    }}
                     className="text-blue-600 hover:underline"
                   >
                     See Detail
                   </Link>
                 </td>
-                <td className="px-5 py-3">
-                  <button className="mr-2 text-[#0456F7] cursor-pointer">
-                    <PencilLine size={16} />
-                  </button>
-                  <button className="text-[#DF0025] cursor-pointer">
-                    <Trash2 size={16} />
-                  </button>
+                <td className="px-0 py-3">
+                  <Dialog open={dialogEditOpen}>
+                    <DialogTrigger asChild>
+                      <Button
+                        className="text-[#0456F7] cursor-pointer bg-transparent hover:bg-transparent shadow-none"
+                        onClick={() => handleEditClick(invoice, idx)}
+                      >
+                        <PencilLine size={16} />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-2xl text-theme [&>button]:hidden p-12 rounded-[32px] space-y-0">
+                      <DialogHeader>
+                        <DialogTitle className="text-3xl">Invoice #{form.invoice_id.toLocaleString()}</DialogTitle>
+                        <DialogDescription className="text-lg">
+                          Edit this invoice with new information.
+                        </DialogDescription>
+                      </DialogHeader>
+
+                      <div className="grid gap-4 py-2 space-y-1">
+                        {/* Product ID */}
+                        <div>
+                          <label className="block text-md font-medium mb-2">
+                            Date
+                          </label>
+                          <div className="flex border-none px-4 w-full text-md h-[48px] items-center rounded-md
+                          bg-gray-100 dark:bg-[#2a2a2a] cursor-not-allowed text-gray-500">
+                            {form.invoice_date
+                              ? format(parseISO(form.invoice_date), 'dd MMMM yyyy')
+                              : '-'}
+                          </div>
+                          {/* <Input
+                            placeholder="Update Invoice Date"
+                            value={invoice.invoice_date}
+                            // onChange={(e) => handleChange("productID", e.target.value)}
+                            readOnly
+                            className="border-none px-3 py-2 w-full text-sm h-[48px] bg-gray-100 dark:bg-[#2a2a2a] cursor-not-allowed text-gray-500"
+                          /> */}
+                        </div>
+
+                        <div>
+                          <label className="block text-sm font-medium mb-2">
+                            Total Amount
+                          </label>
+                          <div className="flex border-none px-4 w-full text-md h-[48px] items-center rounded-md
+                          bg-gray-100 dark:bg-[#2a2a2a] cursor-not-allowed text-gray-500">
+                            {form.total_price.toLocaleString()}
+                          </div>
+                        </div>
+
+                        {/* Product Name */}
+                        <div>
+                          <label className="block text-md font-medium mb-2">
+                            Amount Paid
+                          </label>
+                          <Input
+                            placeholder="Update amount paid"
+                            value={form.amount_paid}
+                            className="h-[48px] text-md flex px-4"
+                            style={{ fontWeight: 400, fontSize: 16 }}
+                            onChange={(e) => handleChange("amount_paid", e.target.value)}
+                            required
+                          />
+                        </div>
+
+                        {/* Brand Name */}
+                        <div>
+                          <label className="block text-md font-medium mb-2">
+                            Payment Method
+                          </label>
+                          <Input
+                            placeholder="Update payment method"
+                            value={form.payment_method}
+                            className="h-[48px] text-md flex px-4"
+                            style={{ fontWeight: 400, fontSize: 16 }}
+                            onChange={(e) => handleChange("payment_method", e.target.value)}
+                            required
+                          />
+                        </div>
+
+
+                      </div>
+
+                      <DialogFooter className="mt-1 grid grid-cols-2">
+                        <Button variant="outline" className="rounded-[80px] text-md h-[48px]"
+                          onClick={() => setDialogEditOpen(false)}>
+                          Cancel
+                        </Button>
+                        <Button
+                          onClick={handleUpdateInvoice}
+                          // disabled={!isFormValid}
+                          className="bg-[#0456F7] text-white hover:bg-[#0348CF] rounded-[80px] text-md h-[48px]"
+                        >
+                          Update Invoice
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+
+
+                  <Dialog open={dialogDeleteOpen}
+                    onOpenChange={(open) => {
+                      setDialogDeleteOpen(open)
+                      if (!open) setDeleteIndex(null)
+                    }}>
+                    <DialogTrigger asChild>
+                      <Button className="text-[#DF0025] cursor-pointer bg-transparent hover:bg-transparent shadow-none"
+                        onClick={() => setDeleteIndex(idx)}>
+                        <Trash2 size={16} />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="max-w-sm p-12 md:p-12 rounded-[32px] [&>button]:hidden text-center justify-center w-auto"
+                      onEscapeKeyDown={(e) => e.preventDefault()}
+                      onPointerDownOutside={(e) => e.preventDefault()}
+                    >
+                      <DialogHeader>
+                        <DialogTitle className="text-4xl font-medium text-theme text-center">Delete Invoice #{invoice.invoice_id}</DialogTitle>
+                        <DialogDescription className="text-xl font-regular text-center mt-5 w-[340px]">
+                          This action will delete invoice from the database permanently.
+                          Are you sure you want to proceed?
+                        </DialogDescription>
+                      </DialogHeader>
+                      <DialogFooter className="mt-5 flex w-full justify-center text-center mx-auto">
+                        <div>
+                          <Button
+                            onClick={handleDeleteInvoice}
+                            className="text-lg h-[48px] w-full bg-[#DD0004] text-white hover:bg-[#BA0003] rounded-[80px] cursor-pointer text-center">
+                            Delete</Button>
+
+                          <Button variant="outline" className="text-lg mt-4 h-[48px] flex w-[340px] rounded-[80px] text-theme cursor-pointer"
+                            onClick={() => setDialogDeleteOpen(false)}>
+                            Cancel</Button>
+                        </div>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
                 </td>
               </tr>
             ))}
