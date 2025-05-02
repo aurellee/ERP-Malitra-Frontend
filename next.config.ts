5ed77f33bfd5959ec41d050865eef3f950d4a47f
KelXR <kostamanmichael@gmail.com>
2025-05-03 01:43:46 +0700

    add noUnusedLocals

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: you’ll still see errors locally, but the build won’t fail
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
